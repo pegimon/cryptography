647a6e5d0a2de7b1900cb70f56997785e72a830c
pegimon <yousefalaa2279@gmail.com>
2023-05-13 01:35:01 +0300

    Monoalphabetic encryption and decryption implemented

--- Monoalphabetic.py
+++ Monoalphabetic.py
@@ -1,6 +1,17 @@
 def encrypt(plain, key):
-    pass
+    cipher = ''
+    plain = plain.lower()
+    for c in plain:
+        if ord(c) - ord('a') > len(key):
+            raise Exception("invalid key: characters in plain text can't be encrypted by the key.")
+        cipher += key[ord(c) - ord('a')]
+    return cipher
 
 
-def decrypt(plain, key):
-    pass
+def decrypt(cipher, key):
+    plain = ''
+    cipher = cipher.lower()
+    key = key.lower()
+    for c in cipher:
+        plain += chr(ord('a') + key.find(c))
+    return plain
