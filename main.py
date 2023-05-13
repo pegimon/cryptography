6dd55ff2e4834c68aac44fddc7f841a62933d648
pegimon <yousefalaa2279@gmail.com>
2023-05-13 18:46:28 +0300

    added src directory

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import Ceaser
+from src import Monoalphabetic, Ceaser
+from src.PlayFair import Playfair
 
 if __name__ == '__main__':
     print(Ceaser.encrypt("ABC", 2))
     print(Ceaser.decrypt('bcd', 1))
+    print(Monoalphabetic.encrypt("bba", 'BCA'))
+    print(Monoalphabetic.decrypt('CCB', 'bca'))
+    p = Playfair()
+    cipher = p.encrypt("ball", "monarchy")
+    print(p.decrypt(cipher, "monarchy"))
