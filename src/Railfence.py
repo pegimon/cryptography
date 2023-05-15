bd827fbf8ffc7718ce2ca932eb7e1fc4b99bb132
pegimon <yousefalaa2279@gmail.com>
2023-05-16 01:38:06 +0300

    Railfence encrypt and the decrypt commented for later implementation

--- src/Railfence.py
+++ src/Railfence.py
@@ -2,32 +2,51 @@ def encrypt(plain, key):
     cipher = ""
     matrix = []
     idx = 0
+    up_down = True
     for i in range(int(key)):
         matrix.append([])
     for c in plain:
         matrix[idx].append(c)
-        idx = (idx + 1) % int(key)
+        if idx >= key - 1:
+            up_down = False
+        elif idx <= 0:
+            up_down = True
+        if up_down:
+            idx += 1
+        else:
+            idx -= 1
     for i in matrix:
         for j in i:
             cipher += j
     return cipher
 
-
-def decrypt(cipher, key):
-    plain = ""
-    matrix = []
-    idx = 0
-    counter = 0
-    for i in range(int(key)):
-        matrix.append([])
-    for c in cipher:
-        if counter >= len(cipher):
-            idx += 1
-            counter = idx
-        counter += int(key)
-        matrix[idx].append(c)
-    for rowElement in range((len(cipher) // key) + 1):
-        for row in matrix:
-            if rowElement < len(row):
-                plain += row[rowElement]
-    return plain
+#
+# def decrypt(cipher, key):
+#     plain = ""
+#     matrix = []
+#     arr = []
+#     idx = 0
+#     counter = 0
+#     for i in range(int(key)):
+#         matrix.append([])
+#         arr.append(0)
+#     for c in cipher:
+#         if counter >= len(cipher):
+#             idx += 1
+#             counter = idx
+#         counter += 2 * int(key) - 2
+#         matrix[idx].append(c)
+#     idx = 0
+#     up_down = True
+#     while len(plain) < len(cipher):
+#         plain += matrix[idx][arr[idx]]
+#         arr[idx] += 1
+#         if idx >= key - 1:
+#             up_down = False
+#         elif idx <= 0:
+#             up_down = True
+#         if up_down:
+#             idx += 1
+#         else:
+#             idx -= 1
+#     return plain
